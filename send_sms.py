72bfd880b44accb472642a7f4a2c98e6b386728e
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-19 22:08:16 +0530

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,8 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twilio
+
+client = Client(account_sid,auth_token)
+
+my_message = "Hey there! I love Twilio!"
+
+message = client.messages.create(to=my_cell,from_=my_twilio, body=my_message)
